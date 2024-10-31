96619b4f4e0f6d865e849eaefef12b13f1803f42
Alex Gold <88856796+goonyboy@users.noreply.github.com>
2024-10-31 13:48:10 +0300

    Add files via upload
    update

--- frontend/script/api.js
+++ frontend/script/api.js
@@ -1,27 +1,30 @@
-// api.js
 const BASE_URL = 'http://37.252.5.123:8000/words';
+
+// Функция для отправки слов на сервер
 export const sendWords = async (rus_word, eng_word) => {
     try {
         const response = await axios.post(`${BASE_URL}`, {
             rus_word: rus_word,
             eng_word: eng_word
         });
-        return response.data; // Возвращаем данные ответа
+        return response.data;
     } catch (error) {
         console.error('Error posting data:', error);
-        throw error; // Пробрасываем ошибку для обработки в app.js
+        throw error;
     }
 };
 
-// Функция для получения слов
-export function getWords(page) {
-    return axios.get(`${BASE_URL}?page=${page}`);
+// Функция для получения слов с учетом пагинации и фильтрации
+export function getWords(page, wordsPerPage, wordFilter = '') {
+    const query = wordFilter ? `&word_filter=${encodeURIComponent(wordFilter)}` : '';
+    return axios.get(`${BASE_URL}?page=${page}&size=${wordsPerPage}${query}`);
 }
 
 // Функция для удаления слова
 export function deleteWordFromServer(wordId) {
     return axios.delete(`${BASE_URL}/${wordId}`);
 }
+
 
 // Функция которая передает рандомные слова в карточку
 export const getRandomWords = async () => {
